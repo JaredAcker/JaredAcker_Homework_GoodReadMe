63db2a73f7020c8b78ff09db88f8daca211d62b8
JaredAcker <jaredacker1@gmail.com>
2021-09-28 20:29:35 -0400

    Update index.js
    added in a const for questions

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateFile = require('./util/generateFile')
+
+const questions = [
+    {
+        type: 'input', 
+        name:'title',
+        message: "Name your project's title"
+    },
+
+]
